56c5dfdb0a0bda700663b3fe62c34dd9b87cd2d6
Redsskull <ivry91@gmail.com>
2024-01-13 18:15:42 +0100

    initial commit. set up backend user model and test

---
+++ accounts/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+from typing import Any, Optional
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.utils import timezone
+
+class CustomUserManager(UserManager):
+    """
+    I found that in order to allow special users special access to see specific planes, I had to create my own auth app, which is called account. 
+    This class is to be able to manage the creation of users in this model, and return a user to the User class that follows it.
+
+    """
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("You have not provided a valid email address")
+        
+        email =self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+    
+    def create_user(self, email=None, password=None,  **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email=None, password=None,  **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+    
+
+#creating the user model based on the fields above. 
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(blank=True, default='', unique=True)
+    name = models.CharField(max_length=200, blank=True, default='')
+
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank = True, null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['password']
+
+    class Meta:
+        verbose_name = 'User'
+    
+    def get_full_name(self):
+        return self.name
+    
+    def get_short_name(self):
+        return self.name or self.email.split('@')[0]
+
