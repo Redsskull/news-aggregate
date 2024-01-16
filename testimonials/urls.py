ab5a7d8e7f6a1ac00877fb594c122d76d7b41438
Redsskull <ivry91@gmail.com>
2024-01-16 18:04:24 +0100

    create full CRUD functionality for the testimonials model

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import TestimonialListView, AddTestimonialView
+from .views import TestimonialListView, AddTestimonialView,EditTestimonialView, DeleteTestimonialView
 
 app_name = 'testimonials' 
 
 urlpatterns = [
-    path('list/', TestimonialListView.as_view(), name='testimonials_list'),  # Corrected TestimonialListView
-    path('leave/', AddTestimonialView.as_view(), name='testimonials_leave'),  # Corrected AddTestimonialView
-    path('add/', AddTestimonialView.as_view(), name='add_testimonial'),  # Corrected AddTestimonialView
+    path('list/', TestimonialListView.as_view(), name='testimonials_list'), 
+    path('leave/', AddTestimonialView.as_view(), name='testimonials_leave'),  
+    path('add/', AddTestimonialView.as_view(), name='add_testimonial'),
+    path('<int:testimonial_id>/edit/', EditTestimonialView.as_view(), name='edit_testimonial'),
+    path('<int:testimonial_id>/delete/', DeleteTestimonialView.as_view(), name='delete_testimonial'),  
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -4,12 +4,16 @@ from .models import Testimonial
 from .forms import TestimonialForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseForbidden
+from django.contrib import messages
+
 
 class TestimonialListView(View):
     template_name = 'testimonials_list.html'
 
     def get(self, request):
-        testimonials = Testimonial.objects.filter(approved=True)
+        testimonials = Testimonial.objects.filter(approved=False)
         return render(request, self.template_name, {'testimonials': testimonials})
 
 class AddTestimonialView(View):
@@ -27,5 +31,35 @@ class AddTestimonialView(View):
             return redirect('testimonials:testimonials_list')
         return render(request, self.template_name, {'form': form})
 
+
+class EditTestimonialView(View):
+    template_name = 'edit_testimonial.html'
+
+    def get(self, request, testimonial_id):
+        testimonial = get_object_or_404(Testimonial, id=testimonial_id, user=request.user)
+        form = TestimonialForm(instance=testimonial)
+        return render(request, self.template_name, {'form': form, 'testimonial': testimonial})
+
+    def post(self, request, testimonial_id):
+        testimonial = get_object_or_404(Testimonial, id=testimonial_id, user=request.user)
+        form = TestimonialForm(request.POST, instance=testimonial)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Testimonial updated successfully.')
+            return redirect('testimonials:testimonials_list')
+        return render(request, self.template_name, {'form': form, 'testimonial': testimonial})
+
+class DeleteTestimonialView(View):
+    template_name = 'delete_testimonial.html'
+
+    def get(self, request, testimonial_id):
+        testimonial = get_object_or_404(Testimonial, id=testimonial_id, user=request.user)
+        return render(request, self.template_name, {'testimonial': testimonial})
+
+    def post(self, request, testimonial_id):
+        testimonial = get_object_or_404(Testimonial, id=testimonial_id, user=request.user)
+        testimonial.delete()
+        messages.success(request, 'Testimonial deleted successfully.')
+        return redirect('testimonials:testimonials_list')
 
 
